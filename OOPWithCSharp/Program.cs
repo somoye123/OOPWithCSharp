98ec35aa55c8dfd062e7b4dd199b509fd276e623
somoye123 <somoye.ayotunde@gmail.com>
2021-01-04 21:36:15 +0100

    explain variable naming convention as a comment

--- OOPWithCSharp/Program.cs
+++ OOPWithCSharp/Program.cs
@@ -8,6 +8,9 @@ namespace OOPWithCSharp
 		{
 			// variables are declared once and can be changed later on
 
+			// Naming of variables or constant start with a letter
+			// followed by a number or an underscore(_)
+
 		}
 	}
 }
