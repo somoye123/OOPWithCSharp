34c52fc6cdb5fb334f1c9edee7f997621f1d1437
somoye123 <somoye.ayotunde@gmail.com>
2021-01-04 21:38:15 +0100

    add three  variables to experiment naming convention

--- OOPWithCSharp/Program.cs
+++ OOPWithCSharp/Program.cs
@@ -10,7 +10,9 @@ namespace OOPWithCSharp
 
 			// Naming of variables or constant start with a letter
 			// followed by a number or an underscore(_)
-
+			int myInt = 1;
+			int my_Int = 2;
+			int myInt1 = 3;
 		}
 	}
 }
