3889d3010b81785b10415f2a0c7fc8c57e095b02
somoye123 <somoye.ayotunde@gmail.com>
2021-01-04 21:40:03 +0100

    add definition of a constant as a comment and created a constant variable

--- OOPWithCSharp/Program.cs
+++ OOPWithCSharp/Program.cs
@@ -13,6 +13,9 @@ namespace OOPWithCSharp
 			int myInt = 1;
 			int my_Int = 2;
 			int myInt1 = 3;
+
+			// constant are declared once and can not be changed later on
+			const int A_CONST_INT = 100;
 		}
 	}
 }
