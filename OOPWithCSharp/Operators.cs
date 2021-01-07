4cff062c0d64635509f586f57407a3d22b953ca2
somoye123 <somoye.ayotunde@gmail.com>
2021-01-07 16:32:43 +0100

    add definition of an operator as a comment

--- OOPWithCSharp/Operators.cs
+++ OOPWithCSharp/Operators.cs
@@ -4,5 +4,7 @@ public class Operators
 {
 	static void Main(string[] args)
 	{
+		// What are operators?
+		// operators are characters that allow for certain action or operations to take place
 	}
 }
