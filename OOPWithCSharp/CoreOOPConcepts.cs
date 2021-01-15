cb2279f0e21d1904129e20a2caeb345a5da1f68d
somoye123 <somoye.ayotunde@gmail.com>
2021-01-15 23:53:52 +0100

    add properties class can contain as comment

--- OOPWithCSharp/CoreOOPConcepts.cs
+++ OOPWithCSharp/CoreOOPConcepts.cs
@@ -7,6 +7,9 @@ public class CoreOOPConcepts
 	// they allow the user to create their own type as objects
 	public class Monster
 	{
+		//what can a class contain
+		// data members - constanst/variables
+		//methods/functions
 	}
 	static void Main(string[] args)
 	{
