7f6afd6f6848b9e59e7911ded95923ee1aceda39
somoye123 <somoye.ayotunde@gmail.com>
2021-01-15 23:50:25 +0100

    add as comment the definition of a class

--- OOPWithCSharp/CoreOOPConcepts.cs
+++ OOPWithCSharp/CoreOOPConcepts.cs
@@ -2,6 +2,9 @@
 
 public class CoreOOPConcepts
 {
+	//what are classes?
+	//blueprints to creating an object
+	// they allow the user to create their own type as objects
 	static void Main(string[] args)
 	{
 	}
