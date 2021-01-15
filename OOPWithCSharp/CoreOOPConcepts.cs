9628aabde979f390eed7e371da4ca775943b196d
somoye123 <somoye.ayotunde@gmail.com>
2021-01-15 23:57:39 +0100

    add definition of different OOP access level

--- OOPWithCSharp/CoreOOPConcepts.cs
+++ OOPWithCSharp/CoreOOPConcepts.cs
@@ -10,6 +10,13 @@ public class CoreOOPConcepts
 		//what can a class contain
 		// data members - constanst/variables
 		//methods/functions
+
+		//public class - can be accessed anywhere on this project
+		//public data members/methods - can be accessed anywhere the class was instantiated
+
+		//private data members/methods - can only be accessed in the class
+
+		//static data members/methods - apply to the entire class rather than an instance of it
 	}
 	static void Main(string[] args)
 	{
