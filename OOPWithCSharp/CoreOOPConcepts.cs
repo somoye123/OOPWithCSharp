81981a344235e45617063b0f8870b6a556f620df
somoye123 <somoye.ayotunde@gmail.com>
2021-01-15 23:59:12 +0100

    add examples of public, private and static members

--- OOPWithCSharp/CoreOOPConcepts.cs
+++ OOPWithCSharp/CoreOOPConcepts.cs
@@ -13,10 +13,14 @@ public class CoreOOPConcepts
 
 		//public class - can be accessed anywhere on this project
 		//public data members/methods - can be accessed anywhere the class was instantiated
+		public string name_;
+		public int size_;
 
 		//private data members/methods - can only be accessed in the class
+		private int scare_;
 
 		//static data members/methods - apply to the entire class rather than an instance of it
+		private int nMonsters_;
 	}
 	static void Main(string[] args)
 	{
