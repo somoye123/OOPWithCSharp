1175e033c32c6958c187d1b8496b27ad54998904
somoye123 <somoye.ayotunde@gmail.com>
2021-01-15 23:51:29 +0100

    create a new Monster class

--- OOPWithCSharp/CoreOOPConcepts.cs
+++ OOPWithCSharp/CoreOOPConcepts.cs
@@ -5,6 +5,9 @@ public class CoreOOPConcepts
 	//what are classes?
 	//blueprints to creating an object
 	// they allow the user to create their own type as objects
+	public class Monster
+	{
+	}
 	static void Main(string[] args)
 	{
 	}
