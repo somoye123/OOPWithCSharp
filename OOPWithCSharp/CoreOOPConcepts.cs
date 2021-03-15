6be8fa9b0c7a9d8eef6fd33d5c55ee970e738c33
somoye123 <somoyeayotunde@gmail.com>
2021-03-15 13:58:34 +0100

    complete PartB of classes and additional types

--- OOPWithCSharp/CoreOOPConcepts.cs
+++ OOPWithCSharp/CoreOOPConcepts.cs
@@ -15,12 +15,13 @@ public class CoreOOPConcepts
 		//public data members/methods - can be accessed anywhere the class was instantiated
 		public string name_;
 		public int size_;
+		public const int legs_ = 2;
 
 		//private data members/methods - can only be accessed in the class
 		private int scare_;
 
 		//static data members/methods - apply to the entire class rather than an instance of it
-		private int nMonsters_;
+		public static int nMonsters_;
 
 		//default constructor -initiated all values
 		public Monster()
@@ -30,6 +31,22 @@ public class CoreOOPConcepts
 			scare_ = 20;
 			nMonsters_++; //increment number of monsters
         }
+
+		//constructor 
+		public Monster(string name,int size, int scare) {
+			name_ = name;
+			size_ = size;
+			scare_ = scare;
+			nMonsters_++;
+		}
+
+		//methods
+		public void print()
+        {
+			Console.WriteLine(name_);
+			Console.WriteLine(size_);
+			Console.WriteLine(scare_);
+		}
 	}
 	static void Main(string[] args)
 	{
@@ -40,7 +57,22 @@ public class CoreOOPConcepts
 		Dinosour.name_ = "Rex";
 		Console.WriteLine(Dinosour.name_);
 
+		// change a const cannot be changed
+		// example -- Dinosour.legs_ = 3;
+
 		//private members cannot be accessed here
 		// example -- Console.WriteLine(Dinosour.scare_);
+
+		//using non-default constructor
+		Monster Dragon = new Monster("Drako", 30, 25);
+		Console.WriteLine(Dragon.name_);
+
+		//static check
+		Console.WriteLine(Monster.nMonsters_);
+
+		Monster Zombie = new Monster("Zombie",6,8);
+		//call print method
+		Zombie.print();
+
 	}
 }
